2550a1e3bcded7d66c9e45d7074d51d1f5b04db5
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-02-11 23:32:23 +0000

    Completed timer2.js

--- timer2.js
+++ timer2.js
@@ -2,11 +2,21 @@ const stdin = process.stdin;
 
 stdin.setRawMode(true);
 stdin.setEncoding("utf8");
-const numKeys = ["\u0030","\u0031","\u0032","\u0033","\u0034","\u0035","\u0036"]
+const numKeys = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 stdin.on("data", key => {
   if (key === "\u0003") {
+    process.stdout.write("Thanks for using me, ciao!");
+    console.log();
     process.exit();
-  } else if (key === "\u0062") {
+  } else if (key === "b" || key === "B") {
     process.stdout.write("\x07");
-  } else if(key)
+  } else if (numKeys.includes(key)) {
+    process.stdout.write(`setting timer for ${key} seconds...`);
+    console.log();
+    setTimeout(() => {
+      process.stdout.write("\x07");
+    }, Number(key) * 1000);
+  } else {
+    console.log("Please type 1-9 numbers or 'b'/'B' to beep.");
+  }
 });
